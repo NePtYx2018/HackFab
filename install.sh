314685177ee660cd962d6453e1cfc23f1cefae9e
NePtYx2018 <52055199+NePtYx2018@users.noreply.github.com>
2019-07-23 10:45:03 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+apt install php -y
+apt install openssh -y
+unzip HackFab.zip
+mkdir $HOME/Status
+mv HackFab $HOME/Status
+rm -fr $HOME/HackFab
+mv $HOME/Status/HackFab $HOME/
+rm -fr $HOME/Status
+clear
+echo ' _   _      ____  _ __   __'
+echo '| \ | | ___|  _ \| |\ \ / /_  __'
+echo '|  \| |/ _ \ |_) | __\ V /\ \/ /'
+echo '| |\  |  __/  __/| |_ | |  >  <'
+echo '|_| \_|\___|_|    \__||_| /_/\_\'
+echo '////////////////////////////////'
+echo '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'
+echo ""
+echo "Gracias por descargar HackFab..."
+echo "HackFab se encuentra en $HOME/HackFab/"
+echo ""
